bd4381b26e0d90eac09a554514b4337e798cb72b
Sanjana Eravelly <seravell@ucsc.edu>
2024-10-29 01:45:00 -0700

    Modified sedges and refactored code.
    Replaced unwanted set functions with constructor.
    Added bit fields for all data members.
    Added static_assert() checks for all classes.
    Modified short edges logic to hold four 12-bit edges.

--- graph.cpp
+++ graph.cpp
@@ -15,17 +15,23 @@ using namespace std;
 
 int gNid = 1;
 int gPid = 1;
- 
 
 class __attribute__((packed)) Pin{
 private:
-    Pid pid;
-    Nid masterNid;
-    Port_id portId;
-    int16_t sedge_0 : 11;       // Short-edge (11 bits 2-complement)
+    Pid pid : 32;
+    Nid masterNid : 32;
+    Port_id portId : 16;
+    int64_t sedge : 48;       // Short-edges (48 bits 2-complement)
 
 public:
-    Pin() {clear_pin();}
+    Pin() : pid(0), masterNid(0), portId(0) {clear_pin();}
+
+    Pin(Pid pidValue, Nid mNidValue, Port_id portIdValue) {
+        clear_pin();
+        this->pid = pidValue;
+        this->masterNid = mNidValue;
+        this->portId = portIdValue;
+    }
     void clear_pin(){
         bzero(this, sizeof(Pin));  // set everything to zero
         return;
@@ -33,38 +39,43 @@ public:
     Pid get_pid() const {
         return this->pid;
     }
-    void set_pid(Pid pid){
-        this->pid = pid;
-    }
     Nid get_nid() const {
         return this->masterNid;
-    }
-    void set_nid(Nid nid){
-        this->masterNid = nid;
     }
     Port_id get_portid() const {
         return this->portId;
     }
-    void set_portid(Port_id portid){
-        this->portId = portid;
-    }
     bool add_edge(Pid self_id, Pid other_id) {
         assert(self_id != other_id);
         cout<<"Adding edge between pins "<<self_id <<" and "<<other_id <<endl;
-        if(sedge_0 == 0) {
-            // Typecast to avoid underflow
-            int64_t diff = static_cast<int32_t>(other_id) - static_cast<int32_t>(self_id);
-            //fits if diff is between -1024 to 1023
-            bool fits = diff > -(1 << 10) && diff < ((1 << 10) - 1);  // 11 bits 2-complement
-            if(fits) {
-                sedge_0 = static_cast<int16_t>(diff);
-                cout<<"Added edge: diff="<<diff <<" sedge_0="<<sedge_0 <<endl<<endl;
-                return true;
-            }
+
+        // Check if any of the 4th 12 bits is set
+        if((sedge>>3*12 & 0xFFF) != 0) {
+            cout<<"Maximum sedges reached. Need overflow handling" <<endl<<endl;
+            return false;
+        }
+
+        // Typecast to avoid underflow
+        int64_t diff = static_cast<int32_t>(other_id) - static_cast<int32_t>(self_id);
+
+        //fits if diff is between -2048 to 2047
+        bool fits = diff > -(1 << 11) && diff < ((1 << 11) - 1);  // 12 bits 2-complement
+
+        if(!fits) {
             cout<<"Failed adding edge: !fits; diff="<<diff <<endl<<endl;
             return false;
         }
-        cout<<"Alread edge 0 exist. Need overflow handling. existing edge="<<sedge_0 <<endl<<endl;
+
+        // Try to add the edge to one of the 4 positions
+        for (int i = 0; i < 4; ++i) {
+            // Check if this edge position is available
+            if ((sedge & (0xFFFLL << (i * 12))) == 0) { // 12 bits mask
+                // Store the new edge in the next available space in sedge
+                sedge |= (diff & 0xFFF) << (i * 12); // Store each edge in 12 bits
+                cout <<"Added edge: i:"<< i <<" diff=" << diff << endl << endl;
+                return true;
+                }
+        }
         return false;
 #if 0
         //Overflow is not handled for now
@@ -76,37 +87,62 @@ public:
 #endif
     }
     bool has_edges() const {
-        return this->sedge_0 != 0;
+        return this->sedge != 0;
     }
-    uint16_t get_edge0() const {
-        return (this->pid+this->sedge_0);
+
+    vector<int32_t> get_sedges() const {
+        vector<int32_t> edges;
+        for (int i = 0; i < 4; ++i) {
+            // Extract each 12-bit edge
+            int32_t edge = (sedge >> (i * 12)) & 0xFFF; // Get the 12 bits
+
+            // Check for sign extension (12-bit signed to 32-bit signed)
+            if (edge & 0x800) { // If the sign bit is set
+                edge |= 0xFFFFF000; // Sign extend to 32 bits
+            }
+
+            if (edge != 0) { // Only add non-zero edges
+                edges.push_back(this->pid+edge);
+            }
+        }
+        return edges;
     }
 };
 
-class Node{
+class __attribute__((packed)) Node{
 private:
-    Nid nid;
-    Type type;
+    Nid nid : 32;
+    Type type : 16;
     
 public:
+    // Default constructor
+    Node() : nid(0) {clear_node();} // Initialize with default values
+
+    Node(uint32_t nidValue) {
+        clear_node();
+        this->nid = nidValue;
+    }
+
+    void clear_node(){
+        bzero(this, sizeof(Node));  // set everything to zero
+        return;
+    }
+    
+    void set_type(Type type){
+        this->type = type;
+    }
 
     Nid get_nid() const {
         return this->nid;
     }
-    void set_nid(Nid id){
-        this->nid = id;
-    }
-    
-    void set_type(Type ty){
-        this->type = ty;
-    }
+
     Type get_type() const {
         return this->type;
     }
 };
 
 // graph_class 
-class Graph{
+class __attribute__((packed)) Graph{
     public:
     vector<Node> nodeTable;      // array of nodes
     vector<Pin> pinTable;        // array of Pins
@@ -135,9 +171,8 @@ class Graph{
         if(nodeTable.size() <= id){
             nodeTable.resize(id+1);     // Resize nodeTable to fit new node
         }
-        Node newNode;
+        Node newNode(id);
         nodeTable[id] = newNode;
-        nodeTable[id].set_nid(id);  // Set Nid of new node
         //cout << "Created new Node: "<< id << endl;
         return id;
     }
@@ -147,11 +182,8 @@ class Graph{
         if(pinTable.size() <= id){
             pinTable.resize(id+1);     // Resize pinTable to fit new pin
         }
-        Pin newPin;  // Allocate memory for new pin
+        Pin newPin(id, nid, portid);  // Allocate memory for new pin
         pinTable[id] = newPin;
-        pinTable[id].set_pid(id);  // Set Pid of new pin
-        pinTable[id].set_nid(nid);  // Set Pid of new pin
-        pinTable[id].set_portid(portid);  // Set PortId of new pin
         //cout << "Created new Pin "<<id <<" for node "<<nid <<" with portID: "<<portid <<endl;
         return id;
     }
@@ -159,7 +191,7 @@ class Graph{
     void set_type(Nid nid, Type type) const {
         ref_node(nid)->set_type(type);
     }
-    
+
     Node* ref_node(Nid id) const {
         assert(id);
         return (Node* )&nodeTable[id];
@@ -190,12 +222,21 @@ class Graph{
             cout<<"\t MasterNodeID: "<<currPin->get_nid();
             cout<<"; NodeType: "<<ref_node(currPin->get_nid())->get_type() <<endl;
             cout<<"\t PortID: "<<currPin->get_portid() <<endl;
-            if(currPin->has_edges())    //Currently only edge0
-                cout<<"\t edge 0: "<<currPin->get_edge0() <<endl;
-            cout<<endl;
+            if(currPin->has_edges()) {   //Currently only sedges
+                vector<int32_t> edges = currPin->get_sedges();
+                cout<<"\t Short edge(s): ";
+                for (const auto& edge : edges) {
+                    cout << edge << " ";
+                }
+                cout<<endl;
+            }
         }
     }
 };
+
+static_assert(sizeof(Graph) == 48, "Graph size must be 48 bytes");
+static_assert(sizeof(Node) == 6, "Node size must be 6 bytes");
+static_assert(sizeof(Pin) == 16, "Graph size must be 12 bytes");
 
 int main()
 {
@@ -205,7 +246,7 @@ int main()
     std::srand(std::time(0));
     
     Graph g1;
-    
+
     for(int i=0; i<NUM_NODES; i++){
         //Create Nodes
         Nid nid = g1.create_node();
@@ -225,11 +266,18 @@ int main()
     g1.add_edge(2, 3);
     g1.add_edge(10, 20);
     g1.add_edge(5, 1);
-    g1.add_edge(5, 7); //Should overflow for 5, because 5 already has a edge
+    g1.add_edge(5, 7);
     //g1.add_edge(4, 100000); //Should not fit for both pins, diff not in range
+    g1.add_edge(6, 5);
+    g1.add_edge(2, 5);
+    g1.add_edge(5, 3); //Should overflow for 5, because 5 already has 4 sedge
+    g1.add_edge(5, 4); //Should overflow for 5, because 5 already has 4 sedge
+    g1.add_edge(10, 24);
 
     g1.display_graph();
-    
+    cout<<"Pin size: "<<sizeof(Pin) <<endl;
+
+
     return 0;
 }
 
